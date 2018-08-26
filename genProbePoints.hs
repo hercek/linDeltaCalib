a90df4bc4fe2170faae255e378bc37b049dc9837
Peter Hercek <phercek@gmail.com>
2018-08-26 10:10:53 +0200

    add the maxima notebooks and the haskell script

---
+++ genProbePoints.hs
@@ -0,0 +1,74 @@
+import System.Random
+
+xyRange :: Int
+xyRange = 96
+
+xyInc :: Int
+xyInc = 12 -- 16 or 12 for coarse or fine
+
+radiusSq :: Int
+radiusSq = 130*130
+
+speed :: Int
+speed = 90*60
+
+height :: Int
+height = 14
+
+main :: IO ()
+main = mkBgn >> go (shuffle 0 points)
+ where
+  go [] = return ()
+  go ((x,y):rest) =
+    if x*x + y*y > radiusSq then go rest else do
+    putStrLn $ mkG0 x y
+    putStrLn "G30" -- modify the probe command here!
+    go rest
+
+  mkBgn = mapM_ putStrLn
+    [ "G28"
+    , "G0 X0 Y0 Z" ++ show height
+    , "G0 X-41.2 Y-23.8 F" ++ show speed
+    , "G0 X0 Y-48"
+    , "G0 X41.2 Y-23.8"
+    , "G0 X41.2 Y23.8"
+    , "G0 X0 Y48"
+    , "G0 X-41.2 Y23.8"
+    , "G0 X0 Y0" ]
+
+  mkG0 x y =
+    showString "G1 X" $ shows x $ showString " Y" $ shows y $ showString " Z" $ show height
+
+
+points :: [(Int,Int)]
+points =
+  let range = [0-xyRange, xyInc-xyRange .. xyRange] in
+  concat $ map (\r -> zip range (repeat r)) range
+
+
+shuffle :: Int -> [a] -> [a]
+shuffle seed lst =
+  let ll = length lst in
+  if ll < 2 then lst else
+  pick lst $ mkRandomSeq (ll-1) (randomRs (0, ll-1) $ mkStdGen seed) ll
+
+
+pick :: [a] -> [Int] -> [a]
+pick rv@[] _ = rv
+pick rv@[_] _ = rv
+pick _ [] = undefined
+pick lst (p:ps) =
+  let (prefix, x:sufix) = splitAt p lst in
+  x : pick (prefix ++ sufix) ps
+
+
+mkRandomSeq :: Int -> [Int] -> Int -> [Int]
+mkRandomSeq maxRandomNumber randomNumbers rvLength = go randomNumbers rvLength
+ where
+  go [] _ = []
+  go _ 0 = []
+  go (r:rs) cnt =
+    let upperLimit = cnt * ((maxRandomNumber+1) `div` cnt) in
+    if r >= upperLimit then go rs cnt else
+    (r `rem` cnt) : go rs (cnt-1)
+
